4e51936d710ea5a4141e6ab9b8532f62fd787a37
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-08-31 23:26:32 +0530

    refactor: improve formatting of Notes constant array for better readability

--- src/features/Notes/constant/Notes.constant.ts
+++ src/features/Notes/constant/Notes.constant.ts
@@ -22,22 +22,24 @@ let Notes_Constant_Array: Notes_Type = [
   {
     _id: '4',
     title: 'Express.js Notes',
-    Notes_Image: 'https://user-images.githubusercontent.com/11978772/40430986-a0eb7b92-5e63-11e8-80eb-43fe07f664a6.png',
+    Notes_Image:
+      'https://user-images.githubusercontent.com/11978772/40430986-a0eb7b92-5e63-11e8-80eb-43fe07f664a6.png',
     download_link: 'https://example.com/express-notes.pdf',
   },
   {
     _id: '5',
     title: 'TypeScript Notes',
-    Notes_Image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F4c%2FTypescript_logo_2020.svg%2F1200px-Typescript_logo_2020.svg.png&f=1&nofb=1&ipt=0f1a1e32d1b7d584e8839a147e0505f16a07d1e3f45b591d03d6ce6258b69719',
+    Notes_Image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F4c%2FTypescript_logo_2020.svg%2F1200px-Typescript_logo_2020.svg.png&f=1&nofb=1&ipt=0f1a1e32d1b7d584e8839a147e0505f16a07d1e3f45b591d03d6ce6258b69719',
     download_link: 'https://example.com/typescript-notes.pdf',
   },
   {
     _id: '6',
     title: 'React Notes',
-    Notes_Image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%2Fid%2FOIP.dJTqBQy3mMnMdI8klN_kHAHaGT%3Fr%3D0%26pid%3DApi&f=1&ipt=5c331e3d560cc8092315f429703475ad6068fa15b216ea402dc657b49068aeb3',
+    Notes_Image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%2Fid%2FOIP.dJTqBQy3mMnMdI8klN_kHAHaGT%3Fr%3D0%26pid%3DApi&f=1&ipt=5c331e3d560cc8092315f429703475ad6068fa15b216ea402dc657b49068aeb3',
     download_link: 'https://example.com/react-notes.pdf',
-  }
-
+  },
 ];
 
 export default Notes_Constant_Array;
